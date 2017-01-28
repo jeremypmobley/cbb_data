5497fa4db8805f38138a4724c2e06e28dfb673a9
jeremypmobley <jeremypmobley@gmail.com>
2017-01-28 07:18:08 -0600

    Updated result plotter to include 2 year plot

--- R/kaggle_result_plotter.R
+++ R/kaggle_result_plotter.R
@@ -11,10 +11,11 @@ kaggle_results_df <- read.csv("~/Github/cbb_data/data/kaggle_results_df.csv")
 ### PLOT THE RESULTS ###
 
 # only plot top 100 scores
-top_num_to_plot <- 100
+top_num_to_plot <- 250
 #plot(kaggle_results_df$score[1:top_num_to_plot])
 
 years <- c(2014, 2015, 2016)
+
 
 # plot all three years
 par(mfrow=c(1,3)) 
@@ -27,6 +28,26 @@ for (year in years){
       
        )
 }
+
+
+
+
+
+#plot last two years with my submission
+two_years <- c(2015, 2016)
+
+par(mfrow=c(1,length(two_years))) 
+for (year in two_years){
+  leaderboard_df <- kaggle_results_df[kaggle_results_df$year == year,]
+  plot(leaderboard_df$score[1:top_num_to_plot], 
+       ylab = "logloss", 
+       xlab = "", 
+       ylim = c(0.4,0.6), main = year
+  )
+  #abline(v = 10, col="blue")
+}
+
+
 
 
 
